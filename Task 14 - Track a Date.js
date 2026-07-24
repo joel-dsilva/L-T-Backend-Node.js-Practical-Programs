302c530de15c6df2a10b306763a0238dc9c5c004
Joel Anthony Dsilva <joeldsilva2006@gmail.com>
2026-07-24 13:02:23 +0530

    Update Task 14 - Track a Date.js

--- Task 14 - Track a Date.js
+++ Task 14 - Track a Date.js
@@ -2,3 +2,6 @@
 
 let admissionDate = new Date(2023, 5, 15); // Month is 0-indexed, so 5 = June 
 console.log("Admission Date:", admissionDate.toDateString());
+
+// Output:
+// Admission Date: Thu Jun 15 2023
