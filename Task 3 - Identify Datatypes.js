e984e44337e67058b2f4c6cd7ed6ea733cb049d4
Joel Anthony Dsilva <joeldsilva2006@gmail.com>
2026-07-24 12:57:54 +0530

    Update Task 3 - Identify Datatypes.js
    Added output comments to indicate data types.

--- Task 3 - Identify Datatypes.js
+++ Task 3 - Identify Datatypes.js
@@ -6,3 +6,8 @@ let isPassed = true;            // Boolean
 console.log("studentMarks is a", typeof studentMarks);
 console.log("studentName is a", typeof studentName);
 console.log("isPassed is a", typeof isPassed);
+
+// Output:
+// studentMarks is a number
+// studentName is a string
+// isPassed is a boolean
