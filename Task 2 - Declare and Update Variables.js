107b9c60cd817e5aaaa5243c02bbc82afb46dc30
Joel Anthony Dsilva <joeldsilva2006@gmail.com>
2026-07-24 12:58:45 +0530

    Update Task 2 - Declare and Update Variables.js

--- Task 2 - Declare and Update Variables.js
+++ Task 2 - Declare and Update Variables.js
@@ -4,3 +4,7 @@ let totalStudents = 5;
  
 console.log("System Name:", systemName);
 console.log("Total Students:", totalStudents);
+
+// Output:
+// System Name: Student Record System
+// Total Students: 5
